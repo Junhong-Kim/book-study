5da8f3ed2117c57866ba863defe1fe9349ea17b1
Junhong-Kim <jhk1005jhk@gmail.com>
2020-01-01 16:24:30 +0900

    init

---
+++ src/test/java/kim/junhong/bookstudy/BookStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package kim.junhong.bookstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
